8bf8c0cdd6a7204cee2cb583ef7a2a3b14b2991e
Jari Hämäläinen <nuumiofi@gmail.com>
2020-01-26 15:04:17 +0200

    nuumio: Fix "sdio defer lock" releasing in rockchip_pcie_probe
    RK code from last summed added this to rockchip_pcie_probe():
    
      if (rockchip->dma_trx_enabled == 0)
        return 0;
    
    That effectively skipped releasing the "sdio defer lock" and for
    some reason it doesn't timeout either. Then it looked like wifi
    drivers got tired and gave up waiting for sdio to get up so
    there was no wifi available :D
    
    dmesg from "no wifi/sdio" for reference: https://pastebin.com/Rd3BDnGv
    
    This commit fixes that "dma_trx_enabled check -> return 0".
    No idea why "sdio defer lock" doesn't timeout.
    
    Tested with RP64 + pcie sata controller + Wifi/BT module combo
    on Recalbox.

--- drivers/pci/host/pcie-rockchip.c
+++ drivers/pci/host/pcie-rockchip.c
@@ -1928,7 +1928,7 @@ static int rockchip_pcie_probe(struct platform_device *pdev)
 	}
 
 	if (rockchip->dma_trx_enabled == 0)
-		return 0;
+		goto end_probe;
 
 	rockchip->dma_obj = rk_pcie_dma_obj_probe(dev);
 	if (IS_ERR(rockchip->dma_obj)) {
