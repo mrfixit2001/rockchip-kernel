299bff40c9656e867aa4fbbdf27697f523cc4079
MrFixIt2001 <mrfixit2001@gmail.com>
2021-02-03 11:58:40 -0500

    Update to 4.4.254

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -332,8 +332,11 @@ int usb_function_activate(struct usb_function *function)
 		status = -EINVAL;
 	else {
 		cdev->deactivations--;
-		if (cdev->deactivations == 0)
+		if (cdev->deactivations == 0) {
+			spin_unlock_irqrestore(&cdev->lock, flags);
 			status = usb_gadget_activate(cdev->gadget);
+			spin_lock_irqsave(&cdev->lock, flags);
+		}
 	}
 
 	spin_unlock_irqrestore(&cdev->lock, flags);
